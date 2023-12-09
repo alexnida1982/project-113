573cee4faf8373f57c9b6d9ae7b7809424b4dd28
alexnida1982 <79065467+alexnida1982@users.noreply.github.com>
2023-12-09 13:09:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+}
+
+
+function setup() { 
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+}
+function draw(){
+    circle(30, 30, 20);
+    circle(30, 30, 10);
+    circle(30, 20, 20);
+    circle(60, 20, 20);
+    circle(60, 40, 20);
+    circle(90, 30, 20);
+    ellipse(86, 56, 55, 55);
+    ellipse(36, 96, 55, 55);
+    ellipse(46, 46, 55, 55);
+}
